ea5095399abef8ac332da9afc9dbc74497687bf7
aritra0507 <ariitraa05@gmail.com>
2026-06-11 21:46:58 +0530

    Updated socket.io origin

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const server=app.listen(PORT,console.log(`Server Started on PORT ${PORT}`));
 const io=require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://chat-ter.onrender.com",
     }
 });
 
